b3398d8577995db042bbc432f888ec28c367646d
Keith Kester <keith.a.kester.ctr@army.mil>
2024-07-12 12:30:27 -0500

    Enum logic solution

--- src/main/java/io/pivotal/rps/Hand.java
+++ src/main/java/io/pivotal/rps/Hand.java
@@ -1,7 +1,21 @@
 package io.pivotal.rps;
 
 public enum Hand {
-    PAPER,
-    ROCK,
-    SCISSORS
+    PAPER("ROCK"),
+    ROCK("SCISSORS"),
+    SCISSORS("PAPER");
+
+    private String dominateHandName;
+
+    Hand(String dominateHandName) {
+        this.dominateHandName = dominateHandName;
+    }
+
+    public Result resultAgainst(Hand hand) {
+        return this.equals(hand) ? Result.DRAW : getResult(hand);
+    }
+
+    private Result getResult(Hand hand) {
+        return Hand.valueOf(dominateHandName).equals(hand) ? Result.P1_WINS : Result.P2_WINS;
+    }
 }
