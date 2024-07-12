6aaca2b6d7299f4fced8fff1bdac1fb85b076f6e
Keith Kester <keith.a.kester.ctr@army.mil>
2024-07-12 12:38:57 -0500

    added null safety

--- src/main/java/io/pivotal/rps/Rps.java
+++ src/main/java/io/pivotal/rps/Rps.java
@@ -1,18 +1,19 @@
 package io.pivotal.rps;
 
+import java.util.Objects;
+
 import static io.pivotal.rps.Hand.*;
 import static io.pivotal.rps.Result.*;
 
 public class Rps {
     public Result play(Hand p1Hand, Hand p2Hand) {
-        if (p1Hand == p2Hand) {
+        if (Objects.equals(p1Hand, p2Hand)) {
             return DRAW;
         }
-
         return switch(p1Hand) {
-            case ROCK -> p2Hand == SCISSORS ? P1_WINS : P2_WINS;
-            case PAPER -> p2Hand == ROCK ? P1_WINS : P2_WINS;
-            case SCISSORS -> p2Hand == PAPER ? P1_WINS : P2_WINS;
+            case ROCK -> SCISSORS.equals(p2Hand) ? P1_WINS : P2_WINS;
+            case PAPER -> ROCK.equals(p2Hand) ? P1_WINS : P2_WINS;
+            case SCISSORS -> PAPER.equals(p2Hand) ? P1_WINS : P2_WINS;
         };
     }
 }
